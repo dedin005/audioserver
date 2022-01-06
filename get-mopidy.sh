a072b1b759b8b416b8caaf4709c90ab8d6a2ffe5
Charlie Dedinsky <dedin005@umn.edu>
2022-01-05 22:29:11 -0600

    Push Update

---
+++ get-mopidy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
+sudo apt update
+sudo apt install mopidy mopidy-spotify mopidy-mpd
+
